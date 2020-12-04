965e24ae7307df7ecf0bc90b37ebe176952979f7
Sanat Raorane <sanatraorane@outlook.com>
2020-12-04 16:27:11 +0530

    mouse added

--- AndroidApp/app/src/main/java/com/example/android/remotedesktop/Mouse.java
+++ AndroidApp/app/src/main/java/com/example/android/remotedesktop/Mouse.java
@@ -3,12 +3,29 @@ package com.example.android.remotedesktop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Mouse extends AppCompatActivity {
+
+    EditText xcood,ycood;
+    Button msOp;
+    String send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mouse);
+        xcood=(EditText)findViewById(R.id.ed_xcood);
+        ycood=(EditText)findViewById(R.id.ed_ycood);
+        msOp=(Button)findViewById(R.id.btn_msOperation);
+        msOp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send="x="+String.valueOf(xcood.getText())+"&"+"y="+String.valueOf(ycood.getText());
+                netUtils.send(send);
+            }
+        });
     }
 }
--- DesktopApp/DesktopAppProject/src/DesktopAppPack/keyboard.java
+++ DesktopApp/DesktopAppProject/src/DesktopAppPack/keyboard.java
@@ -1,5 +1,18 @@
 package DesktopAppPack;
 
-public class keyboard {
+import java.awt.AWTException;
+import java.awt.Robot;
 
+public class keyboard {
+	keyboard(Integer keyCode)
+	{
+		try {
+			Robot kbRobot=new Robot();
+			kbRobot.keyPress(keyCode);
+			kbRobot.keyRelease(keyCode);
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
