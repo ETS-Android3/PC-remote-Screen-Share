b656f420bf1e38d8d292414cd6bab1ff9031af9f
Sanat Raorane <sanatraorane@outlook.com>
2021-08-25 10:15:59 +0530

    removed redundancy, added comments

--- DesktopApp/DesktopAppProject/src/DesktopAppPack/display.java
+++ DesktopApp/DesktopAppProject/src/DesktopAppPack/display.java
@@ -1,6 +1,10 @@
+/***
+ * @author Sanat Raorane
+ * The display class captures a screenshot every 500ms and sends it to Android device
+ */
+
 package DesktopAppPack;
 
-import java.awt.AWTException;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
@@ -13,15 +17,18 @@ public class display{
 	display()
 	{
 		try {
+			//Create an object of Java Robot class
 			Robot dispCapture=new Robot();
+			//Create a Rectangle objects with dimensions of screen size
 			Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+			//Pass the Rectangle object to Robot, this will return a BufferedImage 
 			BufferedImage screenBufferImage = dispCapture.createScreenCapture(screenRect);
 			ByteArrayOutputStream bStream=new ByteArrayOutputStream();
-			ImageIO.write(screenBufferImage,"JPG", bStream);
-			byte[] tempArray=bStream.toByteArray();
-			while(main.sendStatus!=1) //changed from finalArray to tempArray
-			{}
-			main.sendObject=tempArray;
+			//Write the buffered image to the ByteArrayOutputStream
+			ImageIO.write(screenBufferImage,"JPG", bStream);  //Warning JPG compression may be lossy
+			byte[] tempArray=bStream.toByteArray();  //Get a byte array object representing the image
+			while(main.sendStatus!=1) {} //Wait for connection establishment
+			main.sendObject=tempArray;     
 			main.sendStatus=0;
 		} catch (Exception e) {
 			e.printStackTrace();
